c82aa11280db5da0938cd2998b9a9e4fe9091c71
Abdelkarim Achlaih <abdelkarima46@gmail.com>
2026-03-31 11:39:05 +0000

    Serve Email

--- index.js
+++ index.js
@@ -6,14 +6,45 @@ var app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
+var { registerTestRoutes } = require('w4j-test-utils');
+registerTestRoutes(app);
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+/*
+Your code Goes Here
+*/
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Listen on port set in environment variable or default to 3000
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log('Your app is listening on port ' + port)
